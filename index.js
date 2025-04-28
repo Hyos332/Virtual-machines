155cb7cec251941fcf911196a1cb50043db08b95
Luis Felipe Hoyos Vega <luis.hoyos1@alumnos.uneatlantico.es>
2025-04-28 09:44:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola desde la nube con Express!´);
+           });
+
+app.listen(port, () => {
+  console.log('Servidor escuchado en http://Localhost:${port});
+});
+
+           
